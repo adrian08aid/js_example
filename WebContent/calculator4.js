7953fd46129084723fd64b3ae71ad998ca00d636
Student10 <Student10@student10>
2016-09-07 11:07:38 +0800

    Sep 7, 2016

---
+++ WebContent/calculator4.js
@@ -0,0 +1,42 @@
+/**
+ * level 4 : prototype
+ */
+
+function Calculator(){
+	this.sum = 0;
+	this.note = '';
+};
+
+Calculator.prototype.plus = function(value){
+	this.sum += value;
+	this.note += ' + ' + value;
+};
+
+Calculator.prototype.minus = function(value){
+	this.sum -= value;
+	this.note += ' - ' + value;
+};
+
+Calculator.prototype.multiple = function(value){
+	this.sum *= value;
+	this.note += ' * ' + value;
+};
+
+Calculator.prototype.divide = function(value){
+	this.sum /= value;
+	this.note += ' / ' + value;
+};
+
+Calculator.prototype.init = function(value){
+	if(arguments.length == 0){
+		this.sum = 0;
+		this.note = '';
+	}else{
+		this.sum = value;
+		this.note += + value;
+	}
+};
+
+Calculator.prototype.printResult = function(){
+	console.log(this.note + " = " + this.sum);
+}
